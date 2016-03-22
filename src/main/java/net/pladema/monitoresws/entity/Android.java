7c8885734e8d463c750885e27351bf480405a7de
Alejandro Pérez <alecio88@gmail.com>
2016-03-22 11:25:15 -0300

    Adding missing Id generator annotation

--- src/main/java/net/pladema/monitoresws/entity/Android.java
+++ src/main/java/net/pladema/monitoresws/entity/Android.java
@@ -2,6 +2,8 @@ package net.pladema.monitoresws.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -10,6 +12,7 @@ import javax.persistence.Table;
 public class Android {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	protected Long id;
 	@Column(name = "id_vehicle")
 	protected String vehicleId;
